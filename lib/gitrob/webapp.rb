e60cb8d0d45ab95ed73d1d2e5f3f183871240207
Max Timchenko <maxvt@pagerduty.com>
2015-10-23 17:28:20 +0000

    Remove hosted domain restriction

--- lib/gitrob/webapp.rb
+++ lib/gitrob/webapp.rb
@@ -47,10 +47,7 @@ module Gitrob
     end
 
     use OmniAuth::Builder do
-      provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-        {
-          :hd => "pagerduty.com"
-        }
+      provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
     end
 
     get '/' do
